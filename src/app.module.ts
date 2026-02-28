3602d893cbb9cb7b90e09fd624be13c3d598c01d
rodrigojustinodevs <rodrigojustino.devs@gmail.com>
2026-02-28 18:42:20 -0300

    feat:  implementação de schema e conmfiguração de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
